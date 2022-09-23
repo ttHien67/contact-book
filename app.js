081b7713c5a50d05cbf6eab6e5c8ba9dcf87c3d1
ttHien67 <tthanhhien2k1@gmail.com>
2022-09-23 09:25:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error'
+    })
+})
 
  
 module.exports = app;
