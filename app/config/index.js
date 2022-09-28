b70723fa0282d6869d16042504f788d900383741
ttHien67 <tthanhhien2k1@gmail.com>
2022-09-28 21:59:59 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,10 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
+    },
+
+    db: {
+        uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/contactbook'
     }
 };
 
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 
 const app = require('./app');
 
-const config = require('./app/config')
+const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.util');
 
 //start server
-const PORT = config.app.port;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log('Connected to the database!');
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    }catch (error){
+        console.log('Cannot connect to database!', error);
+        process.exit();
+    }
+}
+
+startServer();
